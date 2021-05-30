be8e309f4f906b6d959b0fcfbeac01fadc7c8559
swapnilsparsh <arezona.lucky55@gmail.com>
2021-05-30 16:50:58 +0530

    🥚 Easter Egg ?

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,3 @@
-/*Any fool can write code that a computer can understand. Good programmers write code that humans can understand.*/
 class AppConstsnts {
   static const String endPoint = "https://demo.appwrite.io/v1";
   static const String project = "606d5bc9de604";
