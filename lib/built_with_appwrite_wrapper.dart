5b66041f5d1e0fa224e4d89e0f6c07cb03ffcc10
kekavc24 <kelvinkavisi707@gmail.com>
2022-07-25 20:46:23 +0300

    Added better url check instead of try-catch

--- lib/built_with_appwrite_wrapper.dart
+++ lib/built_with_appwrite_wrapper.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class BuiltWithAppwriteWrapper extends StatelessWidget {
   const BuiltWithAppwriteWrapper({Key? key, required this.child})
@@ -17,12 +18,17 @@ class BuiltWithAppwriteWrapper extends StatelessWidget {
           ),
           const SizedBox(height: 10.0),
           GestureDetector(
-              onTap: () {
-                try {
-                  launchUrl(Uri.parse('https://appwrite.io'));
-                } catch (e) {}
-              },
-              child: SvgPicture.asset('assets/built-with-appwrite-hr.svg')),
+            onTap: () async {
+              //
+              bool canLaunch = await canLaunchUrlString(
+                'https://appwrite.io',
+              );
+              //
+              canLaunch ? launchUrl(Uri.parse('https://appwrite.io')) : null;
+              //
+            },
+            child: SvgPicture.asset('assets/built-with-appwrite-hr.svg'),
+          ),
           const SizedBox(height: 10.0),
         ],
       ),
