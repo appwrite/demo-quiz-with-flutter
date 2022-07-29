07dcef7a97ab3b34de91b368486ad84f5f53ceb2
Steven Nguyen <stnguyen90@users.noreply.github.com>
2022-07-29 11:49:02 -0700

    Fix typo with AppConstants class name

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
-class AppConstsnts {
+class AppConstants {
   static const String endPoint = "https://demo.appwrite.io/v1";
   static const String project = "606d5bc9de604";
-  static const String collection = "606d5bd9626ae";
+  static const String collection = "quiz_questions";
 }
