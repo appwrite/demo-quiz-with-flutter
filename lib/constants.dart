f27acf1d748c04825e7677d604dcbdb5cbdd2bd0
kekavc24 <kelvinkavisi707@gmail.com>
2022-07-30 17:59:17 +0300

    Fixed typo

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,4 @@
-class AppConstsnts {
+class AppConstants {
   static const String endPoint = "https://demo.appwrite.io/v1";
   static const String project = "606d5bc9de604";
   static const String collection = "quiz_questions";
